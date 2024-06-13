6913b9fabb652135ee2f19724c898c9a6257c4de
luiz-henrique-ag <hiquedan@hotmail.com>
2024-06-12 21:21:25 -0300

    add: edit functionality

--- src/container/task-list/components/data-table-row-actions.tsx
+++ src/container/task-list/components/data-table-row-actions.tsx
@@ -11,7 +11,11 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Task } from "@/models/task";
-import { useDeleteTask } from "@/service/mutations";
+import { useDeleteTask, useEditTask } from "@/service/mutations";
+import * as Drawer from "@/components/ui/drawer";
+import { PlusIcon } from "lucide-react";
+import { TaskForm } from "@/components/task/task-form";
+import { useState } from "react";
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
@@ -21,33 +25,67 @@ export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
   const task = row.original as Task;
-
-  const { trigger } = useDeleteTask();
+  const [showDialog, setShowDialog] = useState(false)
+  const { trigger: editTrigger } = useEditTask()
+  const { trigger:deleteTrigger } = useDeleteTask();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-        >
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Abrir menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>Editar</DropdownMenuItem>
-        {/* <DropdownMenuItem>Favorite</DropdownMenuItem> */}
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="text-destructive"
-          onClick={() => {
-            trigger({ id: task.id });
-          }}
-        >
-          Excluir
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+    <Drawer.Drawer open={showDialog} onOpenChange={setShowDialog}>
+      <Drawer.DrawerContent>
+        <Drawer.DrawerHeader></Drawer.DrawerHeader>
+        <div className="container max-w-[800px] py-4">
+          <TaskForm
+            defaultValues={{
+              category: task.category,
+              description: task.description,
+              end: task.end,
+              priority: task.priority,
+              start: task.start,
+              title: task.title
+            }}
+            onSubmit={(values) => {
+               editTrigger({
+                id: task.id,
+                task: {
+                  end: values.end,
+                  priority: values.priority,
+                  start: values.start,
+                  title: values.title,
+                  category: values.category,
+                  description: values.description,
+                  parent_id: values.parent_id
+                }
+              }).then(() => setShowDialog(false))
+            }}
+          />
+        </div>
+      </Drawer.DrawerContent>
+    </Drawer.Drawer>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          >
+            <DotsHorizontalIcon className="h-4 w-4" />
+            <span className="sr-only">Abrir menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[160px]">
+          <DropdownMenuItem onClick={() => setShowDialog(!showDialog)}>Editar</DropdownMenuItem>
+          {/* <DropdownMenuItem>Favorite</DropdownMenuItem> */}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="text-destructive"
+            onClick={() => {
+              deleteTrigger({ id: task.id });
+            }}
+          >
+            Excluir
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
